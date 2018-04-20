cfce39cded044332eee1afd1954f63556ba50b40
Bruce LeCaptain <brulecap@gmail.com>
2018-04-20 14:20:07 -0400

    Filled in empty pages. Added modal images. Created static document folder for resume.

--- apps/home/static/home/js/code.js
+++ apps/home/static/home/js/code.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
 	$(".about").on("click", function() {
-		console.log("click about");
 		$(this).parent().siblings(".detail").toggle("slow");
 		$(this).parent().toggle("slow");
 	})
 	$(".close-about").click(function() {
-		console.log("click close-about with click");
 		$(this).parent().slideUp();
 		$(this).parent().siblings(".links").toggle("slow");
 	})
