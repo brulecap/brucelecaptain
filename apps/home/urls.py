9c2e7aaeb10598adbec7978a3e7b1b6f41a72f97
Bruce LeCaptain <brulecap@gmail.com>
2018-04-25 12:52:32 -0400

    Added favicon and removed old pacman hooks.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.base import RedirectView
 urlpatterns = [
-	url(r'^pacman/', views.pacman, name="pacman"),
+	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/home/images/favicon.ico')),
 	url(r'^code/', views.code, name="code"),
 	url(r'^about/', views.about, name="about"),
 	url(r'^contact/', views.contact, name="contact"),
