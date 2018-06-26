a5123a9bb5302c53d2b0882cf1a62473774c211a
Bruce LeCaptain <brulecap@gmail.com>
2018-06-26 11:16:30 -0400

    Changes to layout. Changed to home page to about and changed some text.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,7 +4,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
 	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/home/images/favicon.ico')),
 	url(r'^code/', views.code, name="code"),
-	url(r'^about/', views.about, name="about"),
 	url(r'^contact/', views.contact, name="contact"),
-	url(r'^$', views.index, name="index")
+	url(r'^$', views.about, name="index")
 ]
