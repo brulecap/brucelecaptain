fbcf216ecdb88aa1882dee538f33067d59087e6c
Bruce LeCaptain <brulecap@gmail.com>
2018-06-26 14:01:16 -0400

    Added brucelecaptain.com to allowed_hosts in settings.py. Made side_nav fixed so it does not scroll with the screen.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lt5e=$xzo4$rf7nf(+wqglp9cg1z$r*imu+zg_$=gmpn4^bvds'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['18.188.163.162']
+ALLOWED_HOSTS = ['18.188.163.162','brucelecaptain.com']
 
 
 # Application definition
