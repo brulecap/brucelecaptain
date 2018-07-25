9b105923bc961c5aa221933c96276043d7d7fbdc
Bruce LeCaptain <brulecap@gmail.com>
2018-07-25 13:29:30 -0400

    Change to way 'About' is displayed on code page.

--- apps/home/static/home/js/code.js
+++ apps/home/static/home/js/code.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 	$(".about").on("click", function() {
-		$(this).parent().siblings(".detail").toggle("slow");
-		$(this).parent().toggle("slow");
+		var id = $(this).parent().attr("id");
+		$("." + id + "_detail").toggle("slow");
 	})
-	$(".close-about").click(function() {
+	$(".close").click(function() {
 		$(this).parent().slideUp();
-		$(this).parent().siblings(".links").toggle("slow");
 	})
 });
