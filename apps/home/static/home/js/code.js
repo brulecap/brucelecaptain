d02e915b85330262ba6efe6131ad66f3a84b33e0
Bruce LeCaptain <brulecap@gmail.com>
2018-07-26 08:36:26 -0400

    Added prevent default to on click event.

--- apps/home/static/home/js/code.js
+++ apps/home/static/home/js/code.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$(".about").on("click", function() {
 		var id = $(this).parent().attr("id");
 		$("." + id + "_detail").toggle("slow");
+		event.preventDefault();
 	})
 	$(".close").click(function() {
 		$(this).parent().slideUp();
